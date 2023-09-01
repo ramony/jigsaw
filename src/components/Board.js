fb6f804c0a5f0c7f2c3b16b9fb07175cdd35f85d
jianwei1 <jianwei1@michaels.com>
2023-09-01 18:20:07 +0800

    update

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,7 +2,6 @@ import { useState, useContext } from "react"
 import { observer } from 'mobx-react';
 
 import AppContext from '../store/appContext';
-import { ImagePath } from '../config/GameConfig';
 
 import '../style/Board.less';
 
@@ -10,29 +9,13 @@ const Board = observer(() => {
 
     const appStore = useContext(AppContext);
 
-    const image = ImagePath;
-
     return (
         <div className="Board">
-
-            <div className="Primary-Picture"><img src={image} ></img></div>
-
             {
                 appStore.squares.map((it, index) => {
-                    let pos = it.y + '-' + (1 + appStore.m) + '-' + it.x + '-' + appStore.n;
-                    let bgPos = it.dy + '-' + (1 + appStore.m) + '-' + it.dx + '-' + appStore.n;
-                    //console.log(pos)
-                    if (index === 0) {
-                        return <div className={"Square Square-" + bgPos}></div>
-                    } else {
-                        return [<div className={"Square Square-" + bgPos}></div>,
-                        <div key={it.key} className={"Square Square-Animate Square-Front Square-" + pos}><img src={image} className={"Image-" + bgPos}></img></div>
-                        ]
-                    }
+                    return <div className={"Square Square-" + it}></div>
                 })
             }
-
-            <div className="Result">{appStore.success ? 'Success' : 'In Progress'}</div>
         </div>
     )
 
--- src/store/appStore.js
+++ src/store/appStore.js
@@ -5,104 +5,10 @@ class AppStore {
 
   squares = [];
 
-  m = 0;
-
-  n = 0;
-
-  success = false;
-
-  started = false;
-
   constructor() {
     makeAutoObservable(this);
-    this.start();
-  }
-
-  start() {
-    this.init(BoardMatrix.rows, BoardMatrix.cols);
-    this.shuffle();
-    this.started = true;
-  }
-
-  init(m, n) {
-    this.m = m;
-    this.n = n;
-    this.squares = [];
-    let value = 0;
-    let key = 0;
-    for (let i = 0; i < m + 1; i++) {
-      for (let j = 0; j < n; j++) {
-        if (i == 0 && j < n - 1) {
-          continue;
-        }
-        this.squares.push({ key: key++, x: j, y: i, dx: j, dy: i, value: value++ })
-      }
-    }
-  }
-
-  moveUp() {
-    let { x, y } = this.squares[0];
-    if (y < this.m) {
-      this.swap(0, this.find(x, y + 1))
-    }
-  }
-
-  moveDown() {
-    let { x, y } = this.squares[0];
-    if (y > 1 || (y === 1 && x === this.n - 1)) {
-      this.swap(0, this.find(x, y - 1))
-    }
-  }
-
-  moveLeft() {
-    let { x, y } = this.squares[0];
-    if (x < this.n - 1) {
-      this.swap(0, this.find(x + 1, y))
-    }
-  }
-
-  moveRight() {
-    let { x, y } = this.squares[0];
-    if (x > 0 && !(x === this.n - 1 && y === 0)) {
-      this.swap(0, this.find(x - 1, y))
-    }
-  }
-
-  swap(i, j) {
-    let { x, y } = this.squares[i];
-    this.squares[i].x = this.squares[j].x;
-    this.squares[i].y = this.squares[j].y;
-    this.squares[j].x = x;
-    this.squares[j].y = y;
-  }
-
-  find(x, y) {
-    for (let i in this.squares) {
-      let square = this.squares[i];
-      if (square.x === x && square.y === y) {
-        return i;
-      }
-    }
-    throw new Error('unexpected error');
-  }
-
-  checkSuccess() {
-    this.success = this.squares.every(it => it.x === it.dx && it.y === it.dy);
-  }
-
-  shuffle() {
-    for (let i = 0; i < ShuffleStrong; i++) {
-      let n = Math.floor(Math.random() * 4);
-      console.log(n)
-      if (n === 0) {
-        this.moveRight();
-      } else if (n === 1) {
-        this.moveDown();
-      } else if (n === 2) {
-        this.moveLeft();
-      } else {
-        this.moveUp();
-      }
+    for (let i = 0; i < 300; i++) {
+      this.squares.push(i % 2)
     }
   }
 
